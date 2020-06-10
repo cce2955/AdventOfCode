b872abebef71dba972bfe87491bdd0a193e2850f
Everett <christopher.everett@henry.k12.ga.us>
2020-06-09 23:58:13 -0400

    This one killed me

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,33 +1,14 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Scanner;
 
-import fuelCalc.FuelCalculation;
-import intCode.OpCode;
+import manhattanDistance.ManhattanDistance;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
+		ManhattanDistance man = new ManhattanDistance();
 		
-		FuelCalculation calc = new FuelCalculation();
-		
-		
-		
-		
-		
-//		int total = 0;
-//		calc.gatherFromSource(calc.file);
-//		for (int i = 0; i < calc.getTotalFuel().size(); i++) {
-//			total += calc.gatherFuelData(calc.getTotalFuel().get(i));
-//		}
-//		System.out.println(total);
+		man.manhattanMain();
 	}
-
 }
